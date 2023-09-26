9f0e7c86597970579f024fcd6db75872bf97983f
Ryan Mattson <ryan.mattson32@gmail.com>
2023-09-25 20:33:28 -0600

    Added JWT into auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,1 +1,9 @@
-// Test
+// Decode Token and Get User Information
+import decode from "jwt-decode";
+
+// Create New Class 
+class AuthService {
+    getprofile (){
+       return decode (this.getToken()); 
+    }
+}
