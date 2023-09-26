8a7b5faaa2a29586c29db11cf017242d209c62e9
Ryan Mattson <ryan.mattson32@gmail.com>
2023-09-25 20:46:11 -0600

    Added comments into Auth.js along with code

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -3,7 +3,44 @@ import decode from "jwt-decode";
 
 // Create New Class 
 class AuthService {
+
     getprofile (){
        return decode (this.getToken()); 
     }
 }
+// Checks if User is Logged in
+loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+}
+// Check if Token is Expired
+isTokenExpired(token) {
+    try {
+        const decoded = decode(token);
+        if (decoded.exp < Date.now() / 1000){
+            return true;
+        } else return false;
+    } catch (err) {
+        return false;
+    }
+}
+
+getToken() {
+    // Retrieves Token from LocalStorage
+    return localStorage.getItem("id_token");
+}
+
+login(idToken) {
+    // Saves to LocalStorage
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+}
+
+logout() {
+    // Clear token
+    localStorage.removeItem("id_token");
+    // Page Reload 
+    window.location.assign("/")
+}
+
+export default new AuthService();
